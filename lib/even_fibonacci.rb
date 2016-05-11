59c2666644689a0c9d3e71ffce254712b7b68e21
Aljosha Novakovic <ollynov@gmail.com>
2016-05-10 21:24:27 -0700

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+=begin
+create array that stores fibonnaci from 1-4M
+i = 0
+iterate through array and new_sequence = fib_sequence[i] + fib_sequence[i+1]
+iterate through array and create sum variable that += the evens
+return sum
+
+
+=end
+
+def even_fibonacci_sum(limit)
+  fib_sequence = [1, 2]
+  i = 1
+  new_fib = 3
+  sum = 0
+  while new_fib <= limit
+    fib_sequence << new_fib
+    new_fib = fib_sequence[i] + fib_sequence[(i + 1)]
+    i += 1
+  end
+
+fib_sequence.each{|even| sum += even if even % 2 == 0}
+
+return sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def sum(limit)
+
+    fib_sequence = [1, 2]
+    i = 1
+    new_fib = 3
+    sum = 0
+    while new_fib <= limit
+      fib_sequence << new_fib
+      new_fib = fib_sequence[i] + fib_sequence[(i + 1)]
+      i += 1
+    end
+
+    fib_sequence.each{|even| sum += even if even % 2 == 0}
+
+    return sum
+  end
+
+
+end
